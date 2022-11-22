ba92767f40265f4641856ec1d37c20badf210cea
thithi7110 <thithi@mushpc.localdomain>
2022-11-22 16:14:50 +0900

    add image

--- pages/App.tsx
+++ pages/App.tsx
@@ -1,8 +1,11 @@
-import style from '../styles//App.module.css'
+import style from '../styles/App.module.css'
 import { useState, useRef, useEffect } from "react";
 import { Canvas, MeshProps, useFrame, useThree } from "@react-three/fiber";
 import { Physics, useSphere } from '@react-three/cannon';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import THREE from 'three';
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { MeshBasicMaterial } from "three/src/materials/MeshBasicMaterial";
 
 const Box = () => {
   const ref = useRef<MeshProps>(null);
@@ -10,42 +13,55 @@ const Box = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [mousePosition, setMousePosition] = useState();
 
-  // useFrame((state) => {
-  //   if (ref && ref.current
-  //     && api) {
-  //     console.log(state.mouse.x);
-  //     // console.log(state.mouse.y);
-  //     //console.log(ref.current.position.x);
-  //     // console.log(ref.current.position.y);
-  //     //console.log(api.rotation);
-  //     console.log(ref);
-  //     setMousePosition(state.mouse.x);
-  //     ref.current.rotation.x = state.mouse.x;
-  //     // api.position.set(state.mouse.x, 0, 0), 
-
-  //     // api.rotation.set(0, 0, (state.mouse.x * Math.PI) / 4)
-
-  //     // api.rotation.set(state.mouse.x,state.mouse.y,0);
-  //   }
-  // }, 0);
-  // useFrame(({ mouse }) => {
-  //   const x = (mouse.x * 10) / 2
-  //   const y = (mouse.y * 5) / 2
-  //   if (ref.current) {
-
-  //     ref.current.rotation.set(-y, x, 0)
-  //   }
-  // })
-
+  const loader = new TextureLoader();
+  const materials = [
+    new MeshBasicMaterial({map: loader.load('/anime/nanati2.jpg')}),
+    new MeshBasicMaterial({map: loader.load('/anime/akira.jpg')}),
+    new MeshBasicMaterial({map: loader.load('/anime/dominator.jpg')}),
+    new MeshBasicMaterial({map: loader.load('/anime/kingxkomugi2.jpg')}),
+    new MeshBasicMaterial({map: loader.load('/mush.png')}),
+    new MeshBasicMaterial({map: loader.load('/anime/arrow.png')}),
+  ];
 
   return (
     <mesh
       onPointerOver={() => setIsHovered(true)}
       onPointerOut={() => setIsHovered(false)}
+      material={materials}
     >
       <camera></camera>
-      <boxBufferGeometry args={isHovered ? [1.2, 1.2, 1.2] : [1, 1, 1]} />
-      <meshLambertMaterial color={isHovered ? 0x44c2b5 : 0x9178e6} />
+      <boxBufferGeometry args={isHovered ? [1.5, 1.5, 1.5] : [1.5, 1.5, 1.5]} />
+      <meshLambertMaterial color={isHovered ? "black" : 0x9178e6} />
+    </mesh>
+  );
+};
+
+
+const Box2 = () => {
+  const ref = useRef<MeshProps>(null);
+  //const [ref, api] = useSphere(() => ({ args: [1.2], mass: 1, material: { restitution: 0.95 } }))
+  const [isHovered, setIsHovered] = useState(false);
+  const [mousePosition, setMousePosition] = useState();
+
+  const loader = new TextureLoader();
+  const materials = [
+    new MeshBasicMaterial({map: loader.load('/engineer/python.webp')}),
+    new MeshBasicMaterial({map: loader.load('/engineer/react.png')}),
+    new MeshBasicMaterial({map: loader.load('/engineer/threeJS.png')}),
+    new MeshBasicMaterial({map: loader.load('/engineer/aws.png')}),
+    new MeshBasicMaterial({map: loader.load('/engineer/Csharp.png')}),
+    new MeshBasicMaterial({map: loader.load('/engineer/docker.png')}),
+  ];
+  return (
+    <mesh
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+      material={materials}
+      position={[2,1.5,1]}
+    >
+      <camera></camera>
+      <boxBufferGeometry args={isHovered ? [1.5, 1.5, 1.5] : [1.5, 1.5, 1.5]} />
+      <meshLambertMaterial color={isHovered ? "black" : 0x9178e6} />
     </mesh>
   );
 };
@@ -71,15 +87,15 @@ const CameraController = () => {
 export default function App() {
   return (
     <>
-      <div style={{ width: "100vw", height: "100vh" }}>
+      <div style={{ width: "100vw", height: "100vh"}}>
         <Canvas
           dpr={2} className={style.canvas}>
-          <Physics iterations={5} gravity={[0, 0, 0]}>
-            <color attach="background" args={[0xf5f3fd]} />
+          <Physics iterations={5} gravity={[0, 0, 0]}>       
             <CameraController />
             <ambientLight intensity={0.5} />
             <directionalLight intensity={0.5} position={[-10, 10, 10]} />
             <Box />
+            <Box2 />
           </Physics>
         </Canvas>
       </div>
